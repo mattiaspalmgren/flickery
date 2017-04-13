c98ec3d8f08aa769706bfffc743391ebfde41388
Mattias Palmgren <mattiaspalmgren92@gmail.com>
2017-04-13 20:34:38 +0200

    Adds promise to jsonp

--- app/js/modules/jsonp.js
+++ app/js/modules/jsonp.js
@@ -1,10 +1,35 @@
-const jsonp = (url, callback) => {
-  const script = document.createElement('script');
-  script.type = 'text/javascript';
-  script.src = url;
-  const s = document.getElementsByTagName('script')[0];
-  s.parentNode.insertBefore(script, s);
-  window.jsonFlickrFeed = callback;
-};
+// const jsonp = (url, callback) => {
+//   const script = document.createElement('script');
+//   script.type = 'text/javascript';
+//   script.src = url;
+//   
+//   window.jsonFlickrFeed = callback;
+// };
 
+// export default jsonp;
+
+const TIMEOUT = 3000;
+const jsonp = (url) => {
+  return new Promise((resolve, reject) => {
+
+    const callbackName = 'jsonFlickrFeed';
+    const timeoutTrigger = window.setTimeout(() => {
+      reject(new Error('Timeout'));
+    }, TIMEOUT);
+
+    window[callbackName] = (data) => {
+      window.clearTimeout(timeoutTrigger);
+      resolve(data);
+    };
+
+    const script = document.createElement('script');
+    script.type = 'text/javascript';
+    script.async = true;
+    script.src = url;
+
+    const s = document.getElementsByTagName('script')[0];
+    s.parentNode.insertBefore(script, s);
+
+  });
+}
 export default jsonp;
